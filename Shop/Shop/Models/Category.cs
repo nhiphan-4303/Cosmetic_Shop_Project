bf7d8c49f366daf81cf69a922ef24454afd104c8
Tran-Mai-Anh <155942841+Tran-Mai-Anh@users.noreply.github.com>
2024-12-21 11:36:28 +0700

    SeedDatabase

---
+++ Shop/Shop/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int InStock { get; set; }
+        public int Status { get; set; } 
+
+    }
+}
